2110c86807780481a41b38c2f0db5e736701ac5c
JakubWier <WierJakub@gmail.com>
2022-03-13 12:51:29 +0100

    Completing first tasks

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,23 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    internal class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name) {
+            Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade) {
+            return base.GetLetterGrade(averageGrade);
+        }
+
+        public override void CalculateStatistics() {
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name) {
+            base.CalculateStudentStatistics(name);
+        }
+    }
+}
